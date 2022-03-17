bd0151e531b9f0c7fb996f0b4187ea14311ea25e
oleg <example@gmail.com>
2022-03-17 09:18:04 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,77 @@
+"use strict" 
+
+/* let number = 5;
+const leftBorderWidth = 1; 
+
+number = 10;
+console.log(number); */
+
+/*  
+const result = confirm('Are you heare?')
+console.log(result)
+ */
+
+/* const answer = prompt('Вам есть 18?', "18");   
+console.log(answer + 5); */
+
+/* +prompt с - один тип данных можно преобразовывать в другой с помощью плюса   */
+/* typeof - проверяет тип данных */
+/* Этот код отобразит модальное окно с текстом, полем для ввода текста и кнопками OK/Отмена. */
+
+
+
+
+
+
+/* 
+const answers = [];
+
+answers[0] = prompt('Как ваше имя?', "");
+answers[1] = prompt('Как ваше фамилия?', "");
+answers[2] = prompt('сколько вам лет?', "");
+
+document.write(answers);
+
+
+ */
+
+
+
+
+/* Интерполяция */
+/* 
+const category = 'toys';
+
+console.log(`https://someulrl.com/${category}/5/`)  - работает только с кривыми ковычками
+
+ */
+
+
+/* const user = "oleg"
+
+alert (`Привет, ${user}`); */
+
+
+/* 
+console.log('arr' + " - object");
+console.log(4 + "5");
+ */
+
+let incr = 10,
+    decr = 10;
+
+incr++
+decr--
+
+console.log(incr);
+console.log(decr);
+
+
+console.log(5%2);
+
+console.log(2*4 == 8);
+
+const isChecked = true,
+      isClose = false;
+
+console.log(isChecked || isClose)
